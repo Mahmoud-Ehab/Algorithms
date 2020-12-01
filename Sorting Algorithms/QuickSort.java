4338f0f11d7b36fe6519168ffd301b3bad8df150
Mahmoud-Ehab <52333211+Mahmoud-Ehab@users.noreply.github.com>
2020-12-01 18:44:31 +0200

    Add files via upload

---
+++ Sorting Algorithms/QuickSort.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class QuickSort
+{
+	
+	/*
+	Author: Mahmoud Ehab
+	Date: 30/11/2020
+	*/
+	//Declare unsorted list variable to be filled with user inputs
+	static int[] unsortedList;
+	
+	public static void main(String[] args) {
+		//Validate Usage
+  		if (args.length == 0) {
+			System.out.println("Valid Usage: (Filename).java (Elements of unsorted list)");
+			System.out.println("example: SelectionSort.java 3 2 4 1");
+			return;
+		}
+		
+		//Initialize the unsortedList variable
+		unsortedList = new int[args.length];
+		for (int i = 0; i < args.length; i++) {
+			unsortedList[i] = Integer.parseInt(args[i]);
+		}
+		
+		//Print the list before and after sort
+		System.out.println("Unsorted: " + printList(unsortedList));
+		sort(unsortedList, 0, unsortedList.length);
+		System.out.println("Sorted: " + printList(unsortedList));
+	}
+	
+	
+	/*QuickSort Recursive Function
+	*p1: The list which needs to be sorted.
+	*p2: The start index of the list.
+	*p3: The end index of the list.
+	*p2 and p3 helps us divide the list easly while recursion.
+	*/
+	public static void sort(int[] list, int start, int end) {
+	
+		//Return from the function if the start is the end
+		if (start >= end - 1)
+			return;
+		
+		int pivot = list[end - 1];
+		
+		int k = start - 1; //Index of the last smaller number we investigate
+		
+		for (int i = start; i < end - 1; i++) {
+			//Shift every value less than the pivot to the left of k
+			if (list[i] <= pivot) {
+				int temp = list[i];
+				list[i] = list[k + 1];
+				list[k + 1] = temp;
+				k++;
+			}
+		}
+		//Put the pivot in its correct place
+		int temp = list[end - 1];
+		list[end - 1] = list [k + 1];
+		list[k + 1] = temp;
+		
+		sort(list, start, k + 1); //Sort left division
+		sort(list, k + 2, end); //Sort right division
+		
+	}
+	
+	//Abstract Function for printing lists items
+	//Convert List into String
+	private static String printList(int[] list) {
+		String temp = "";
+
+		for (int integer : list)
+			temp += integer + " ";
+
+		return temp;
+	}
+}
